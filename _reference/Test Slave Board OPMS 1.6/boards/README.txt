1ddc80d795c197f54b9c3aa748582fa78e8355a6
Hoang Anh <hohoanga@gmail.com>
2026-07-03 10:20:11 +0700

    Initial commit

--- _reference/Test Slave Board OPMS 1.6/boards/README.txt
+++
@@ -1,3 +0,0 @@
-Dat file dinh nghia board tu tao (JSON) cho OPMS 1.6 tai day, vd: opms_slave.json
-Sau do tro 'board = opms_slave' trong platformio.ini.
-Tham khao mau: ../../Smart_PDU_2I0_Test/boards/genericSTM32L151CB.json
--- _reference/Test Slave Board OPMS 1.6/src/drivers/drv_common.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <Arduino.h>
-
-// =====================================================================
-//  Hang so dung chung cho cac driver (ADC). Include boi io.cpp, sensor.cpp.
-// =====================================================================
-static const float ADC_VREF = 3.3f;   // V
-static const int   ADC_MAX  = 4095;   // 12-bit
